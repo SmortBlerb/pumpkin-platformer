b41cb1fa1ea42b0e8753a3cc6cc7aca19cb7aaf9
SmortBlerb <SmortBlerb@gmail.com>
2025-11-30 19:36:38 -0700

    Revert "Finished test level, fixed clone"
    This reverts commit 2c3310f6e5d2941097f3b8dcbb25f730610966ad.

--- bounce_pad.gd
+++ bounce_pad.gd
@@ -1,11 +1,8 @@
 extends Area2D
 
 @export var bounce_strength : float = -750.0
-@onready var animator = $"AnimationPlayer"
 
 func _on_area_entered(area: Area2D) -> void:
-	animator.play("bounce")
-	
 	var body = area.get_parent()
 	if body.ball_state:
 		body.velocity = transform.y * bounce_strength * 1.5
